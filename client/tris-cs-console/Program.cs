4c2b24199e3a81eaa67cf7ea02a1b54ee2e702bb
vano <davidevanoncini2003@gmail.com>
2021-04-15 12:42:16 +0200

    starting client

---
+++ client/tris-cs-console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tris_cs_console
+{
+    class Program{
+        static void Main(string[] args){
+
+            client c = new client();
+            Console.WriteLine("gioco tris client:");
+            bool acceptGame = false;
+            for (int i = 0; i < 3; i++){
+                acceptGame = c.connect();
+                if (acceptGame == true){
+                    break;
+                }
+            }
+            if (acceptGame == true){
+
+            }else{
+                Console.WriteLine("errore alla connessione al server, riprova piú tari o idk, vedi tu ");
+            }
+        }
+    }
+}
---
+++ client/tris-cs-console/client.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tris_cs_console
+{
+    class client
+    {
+        //attributi privati
+        private Socket sender;
+        
+        //attributi pubblici
+
+        //metodi privati
+
+        //metodi pubblici
+        public Tuple connect()
+        {
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+  
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    return new Tuple(false, "error");
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                    return new Tuple(false, "error");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    return new Tuple(false, "error");
+                }
+                this.sender = sender;
+                return new Tuple(true, "nil");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+    }
+}
